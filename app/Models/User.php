d25e72a288743d70bb2375701e9aae57ce0a7212
christianpf <cristianpascuafdez@gmail.com>
2020-12-08 20:42:53 +0100

    Tabla para usuarios completa
    Co-Authored-By: gittheroadjack <ale_romero97@hotmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surnames',
         'email',
         'password',
+        'creditCoins',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -16,11 +17,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surnames');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('creditCoins')->default('5');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
