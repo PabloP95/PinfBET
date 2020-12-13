0ac5d52a2ddf2127f16e8a676b1a065cf0c10427
JordiGM <jordi.guetomatavera@alum.uca.es>
2020-12-13 12:47:37 +0100

    Introducido e incluido la imagen del favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="{{ asset('favicon.png') }}" />
         <link rel="stylesheet" href="/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="/css/font-awesome.min.css"/>
         <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
