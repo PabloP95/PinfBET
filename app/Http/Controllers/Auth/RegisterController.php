c7c59e65d653657bbbc4ff92265b74f30c5c9533
christianpf <cristianpascuafdez@gmail.com>
2020-12-16 18:36:52 +0100

    Corregido error que permite registrar usuarios con números.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'surnames' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'alpha'],
+            'surnames' => ['required', 'string', 'max:255', 'alpha'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -17,7 +17,7 @@ return [
     'active_url'           => 'El campo :attribute no es una URL válida.',
     'after'                => 'El campo :attribute debe ser una fecha posterior a :date.',
     'after_or_equal'       => 'El campo :attribute debe ser una fecha posterior o igual a :date.',
-    'alpha'                => 'El campo :attribute solo puede contener letras.',
+    'alpha'                => 'El campo solo puede contener letras.',
     'alpha_dash'           => 'El campo :attribute solo puede contener letras, números, guiones y guiones bajos.',
     'alpha_num'            => 'El campo :attribute solo puede contener letras y números.',
     'array'                => 'El campo :attribute debe ser un array.',
