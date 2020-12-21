f5a58a4ecccca3caa8e0783c2635a34fbc4894ea
JordiGM <jordi.guetomatavera@alum.uca.es>
2020-12-21 12:48:28 +0100

    Funcionalidad para que el scrollbar este abajo

--- public/js/tabsMessenger.js
+++ public/js/tabsMessenger.js
@@ -3,8 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$('#myTabs a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
+//$('#myTabs a').click(function (e) {
+//  e.preventDefault();
+//  $(this).tab('show');
+//});
 
+function updateScroll(){
+    var element = document.getElementById("charla");
+    element.scrollTop = element.scrollHeight;
+}
+
+//once a second
+setInterval(updateScroll,1000);
