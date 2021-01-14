0c4eef3d9f0a790bbcc5f8eec5785f822219b1dd
JordiGM <jordi.guetomatavera@alum.uca.es>
2021-01-14 12:04:21 +0100

    Corregida la estructura

--- resources/views/apuesta.blade.php
+++ resources/views/apuesta.blade.php
@@ -11,7 +11,7 @@
         <div class="col-sm-2 sidenav">
             @include('anuncios.anuncio1')
         </div>
-        <div class="col-sm-8 text-left">
+        <div class="col-sm-10 text-left">
             <div class="jumbotron jumbotron-fluid">
                 <div class="container row">
                     <div class="col-sm-11">
@@ -30,45 +30,40 @@
                     <h3>Usuarios</h3>
                     <div class="tab-content">
                         <ul class="nav nav-pills nv-stackedarea" role="tablist">
-                        <li role="presentation" class="list-group-item" style="width: 100%; background-color: transparent; border: none">
-                            <form method="POST" action="/apuesta/{{ Auth::user()->id }}/{{Auth::user()->id}}">
-                                @csrf
-                                <button type="submit" name="amigo" value="{{Auth::user()->id}}" class="btn btn-success" style="width: 100%" >YO</button>
-                            </form>
-                        </li>
-                        @if(isset($amigos))
-                        @foreach($amigos as $am)
-                        <li role="presentation" class="list-group-item" style="width: 100%; background-color: transparent; border: none">
-                            <form method="POST" action="/apuesta/{{ Auth::user()->id }}/{{$am->id}}">
-                                @csrf
-                                <button type="submit" name="amigo" value="{{$am->id}}" class="btn btn-success" style="width: 100%" >{{$am->name}}  {{$am->surname1}} {{$am->surname2}}</button>
-                            </form>
-                        </li>
-                        @endforeach
-                        @endif
+                            <li role="presentation" class="list-group-item" style="width: 100%; background-color: transparent; border: none">
+                                <form method="POST" action="/apuesta/{{ Auth::user()->id }}/{{Auth::user()->id}}">
+                                    @csrf
+                                    <button type="submit" name="amigo" value="{{Auth::user()->id}}" class="btn btn-success" style="width: 100%" >YO</button>
+                                </form>
+                            </li>
+                            @if(isset($amigos))
+                            @foreach($amigos as $am)
+                            <li role="presentation" class="list-group-item" style="width: 100%; background-color: transparent; border: none">
+                                <form method="POST" action="/apuesta/{{ Auth::user()->id }}/{{$am->id}}">
+                                    @csrf
+                                    <button type="submit" name="amigo" value="{{$am->id}}" class="btn btn-success" style="width: 100%" >{{$am->name}}  {{$am->surname1}} {{$am->surname2}}</button>
+                                </form>
+                            </li>
+                            @endforeach
+                            @endif
                     </div>
                 </div>
                 <div class="col-sm-8">
                     <h3>Asignaturas</h3>
                     <div class="tab-content">
                         @if(isset($_POST['amigo']))
-                            <ul class="nav nav-pills nv-stackedarea" role="tablist">
+                        <ul class="nav nav-pills nv-stackedarea" role="tablist">
                             @foreach($asignaturas as $a)
                             <li role="presentation" class="list-group-item" style="width: 100%; background-color: transparent; border: none">
                                 <a href="/completarApuesta/{{ Auth::user()->id }}/{{$_POST['amigo']}}/{{$a->cod_asig}}" class="btn btn-success" style="width: 100%" >{{$a->nombre_asig}}</a><br>
                             </li>
                             @endforeach
-                        @endif
+                            @endif
                     </div>
                 </div>
             </div>
         </div>
-            <div class="container">
-            </div>
-        </div>
-        <div class="col-sm-2 sidenav">
-            @include('anuncios.anuncio2')
-        </div>
+    </div>
 </div>
 
 @endsection
