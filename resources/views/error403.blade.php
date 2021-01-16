46ed29319cc08dec86c4ee711a2fa4f9ad21a02d
christianpf <cristianpascuafdez@gmail.com>
2021-01-16 03:57:26 +0100

    Vista para simbolizar el acceso prohibido por URL

---
+++ resources/views/error403.blade.php
@@ -0,0 +1,14 @@
+<!-- SECTION: Menu principal -->
+<!-- SECTION: Menu principal -->
+@extends('layouts.master')
+@section('titulo')
+<title>Error</title>
+@endsection
+<!-- LAYOUT: CENTER -->
+<!-- BLOCK: CENTER -->
+@section('content')
+<div class="container p-3 text-white">
+    <h1> Error 403: Acceso no permitido a este usuario </h1>
+</div>
+
+@endsection
