9d156c2c9cdc416833c99fe36e53d5730bb4f0d6
christianpf <cristianpascuafdez@gmail.com>
2021-01-17 17:14:48 +0100

    Cambiada exportación asignaturas.

--- vExportacion_db/asignatura.sql
+++ vExportacion_db/asignatura.sql
@@ -5,12 +5,6 @@
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 12-01-2021 a las 10:12:54
 -- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
